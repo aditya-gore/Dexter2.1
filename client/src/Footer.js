90a2f14c4a3b01274a4671c2252edfbefbf8c098
aditya gore <aditya.gore@dhwaniris.com>
2022-07-18 12:06:29 +0530

    Dexter pushed to remote

---
+++ client/src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+        From{" "}
+        <a href="http://pro.eattheblocks.com" style={{ color: "white" }}>
+          Dexter
+        </a>
+        , a Decentralized Exchange to trade ERC20 tokens, built on top of
+        Ethereum
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
